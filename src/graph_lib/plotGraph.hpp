095ca5c6d5bbd55a30efd9b677a35174e740764d
rFalque <r.falque@gmail.com>
2019-06-22 17:30:32 +1000

    remove the directional namespace

--- src/graph_lib/plotGraph.hpp
+++ src/graph_lib/plotGraph.hpp
@@ -1,7 +1,14 @@
 /**
- * Author: R. Falque
+ * creates a mesh of small cylinders and spheres to visualize the graph
+ * Inputs:
+ *  nodes:        #N by 3 coordinates of the endpoints of the cylinders
+ *  edges:        #E by 2 coordinates of the endpoints of the cylinders
+ *  nodesColors:  #N by 3 RBG colors per cylinder
+ *  edgesColors:  #E by 3 RBG colors per cylinder
+ *  res:        The resolution of the cylinder (size of base polygon)
+ * Outputs:
+ *  plot the graph in the libigl viewer
  * 
- * plot graph in the libigl viewer
  * by R. Falque
  * 14/02/2019
  **/
@@ -18,27 +25,15 @@
 #include "getMinMax.hpp"
 #include "options.hpp"
 
-namespace directional
+inline bool add_graph (igl::opengl::glfw::Viewer & viewer,
+                        const Eigen::MatrixXd & nodes,
+                        const Eigen::MatrixXi & edges,
+                        const Eigen::MatrixXd & nodes_colors,
+                        const Eigen::MatrixXd & edges_colors,
+                        const double & nodes_radius,
+                        const double & edges_radius,
+                        const int res)
 {
-  // creates a mesh of small cylinders to visualize lines on the overlay of the mesh
-  // Inputs:
-  //  nodes:        #N by 3 coordinates of the endpoints of the cylinders
-  //  edges:        #E by 2 coordinates of the endpoints of the cylinders
-  //  nodesColors:  #N by 3 RBG colors per cylinder
-  //  edgesColors:  #E by 3 RBG colors per cylinder
-  //  res:        The resolution of the cylinder (size of base polygon)
-  // Outputs:
-  //  plot og the graph
-
-  inline bool add_graph (igl::opengl::glfw::Viewer & viewer,
-                         const Eigen::MatrixXd & nodes,
-                         const Eigen::MatrixXi & edges,
-                         const Eigen::MatrixXd & nodes_colors,
-                         const Eigen::MatrixXd & edges_colors,
-                         const double & nodes_radius,
-                         const double & edges_radius,
-                         const int res)
-  {
     Eigen::MatrixXd edges_begin, edges_end;
     Eigen::MatrixXi cols;
     cols.resize(1,3);
@@ -46,11 +41,11 @@ namespace directional
 
     edges_begin.resize(edges.rows(), 3);
     edges_end.resize(edges.rows(), 3);
-    
+
     for(int i=0;i<edges.rows(); i++)
     {
-      edges_begin.row(i) << nodes.row(edges(i,0));
-      edges_end.row(i) << nodes.row(edges(i,1));
+        edges_begin.row(i) << nodes.row(edges(i,0));
+        edges_end.row(i) << nodes.row(edges(i,1));
     }
 
     Eigen::MatrixXd nodes_V, edges_V, concatenated_V;
@@ -72,13 +67,13 @@ namespace directional
     viewer.core.background_color << 1, 1, 1, 1;
 
     return true;
-  };
+};
 
-  inline bool add_graph (igl::opengl::glfw::Viewer & viewer,
-                         const Eigen::MatrixXd & nodes,
-                         const Eigen::MatrixXi & edges,
-                         options opts)
-  {
+inline bool add_graph (igl::opengl::glfw::Viewer & viewer,
+                        const Eigen::MatrixXd & nodes,
+                        const Eigen::MatrixXi & edges,
+                        options opts)
+{
     Eigen::MatrixXd nodes_colors, edges_colors;
 
     nodes_colors = Eigen::MatrixXd::Constant(nodes.rows(),3,0.1);
@@ -98,29 +93,28 @@ namespace directional
     double edges_radius = scale/opts.edges_ratio;
 
     return add_graph (viewer, nodes, edges,nodes_colors, edges_colors, nodes_radius, edges_radius, opts.graph_res);
-  };
+};
 
 
-
-  inline bool plot_graph (const Eigen::MatrixXd& nodes,
-                          const Eigen::MatrixXi& edges,
-                          const Eigen::MatrixXd& nodes_colors,
-                          const Eigen::MatrixXd& edges_colors,
-                          const double& nodes_radius,
-                          const double& edges_radius,
-                          const int res)
-  {
+inline bool plot_graph (const Eigen::MatrixXd& nodes,
+                        const Eigen::MatrixXi& edges,
+                        const Eigen::MatrixXd& nodes_colors,
+                        const Eigen::MatrixXd& edges_colors,
+                        const double& nodes_radius,
+                        const double& edges_radius,
+                        const int res)
+{
     igl::opengl::glfw::Viewer viewer;
     add_graph (viewer, nodes, edges, nodes_colors, edges_colors, nodes_radius, edges_radius, res);
     viewer.launch();
 
     return true;
-  };
-  
-  inline bool plot_graph (const Eigen::MatrixXd & nodes,
-                          const Eigen::MatrixXi & edges,
-                          options opts)
-  {
+};
+
+inline bool plot_graph (const Eigen::MatrixXd & nodes,
+                        const Eigen::MatrixXi & edges,
+                        options opts)
+{
     Eigen::MatrixXd nodes_colors, edges_colors;
 
     nodes_colors = Eigen::MatrixXd::Constant(nodes.rows(),3,0.1);
@@ -140,11 +134,11 @@ namespace directional
     double edges_radius = scale/opts.edges_ratio;
 
     return plot_graph (nodes, edges,nodes_colors, edges_colors, nodes_radius, edges_radius, opts.graph_res);
-  };
+};
 
-  inline bool plot_graph (const Eigen::MatrixXd & nodes,
-                          const Eigen::MatrixXi & edges)
-  {
+inline bool plot_graph (const Eigen::MatrixXd & nodes,
+                        const Eigen::MatrixXi & edges)
+{
     Eigen::MatrixXd nodes_colors, edges_colors;
 
     nodes_colors = Eigen::MatrixXd::Constant(nodes.rows(),3,0.1);
@@ -159,7 +153,7 @@ namespace directional
     int res = 30;
 
     return plot_graph (nodes, edges,nodes_colors, edges_colors, nodes_radius, edges_radius, res);
-  };
-}
+};
+
 
 #endif
