17e4d760891ddc64e69a21acbbbdeff8ee4258d3
rFalque <r.falque@gmail.com>
2019-06-22 14:52:05 +1000

    update main file with simple usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,30 @@
 /**
- * Author: R. Falque
- * 
- * main for testing the embedded deformation implementation
+ * basic usage of the library
  * by R. Falque
- * 14/11/2018
+ * 22/06/2019
  **/
-
-// dependencies
-
-#include <igl/readOBJ.h>
-#include <igl/opengl/glfw/Viewer.h>
 
 #include "graph_lib/graphStructure.hpp"
 #include "graph_lib/readGraphOBJ.hpp"
 #include "graph_lib/plotGraph.hpp"
-
 #include "graph_lib/options.hpp"
 
 #include <yaml-cpp/yaml.h>
-
-
 
 int main(int argc, char* argv[])
 {
   options opts;
   opts.loadYAML("../config.yaml");
 
-  /* 
-    * N: nodes of the graph
-    * E: edges of the graph
-    */
-  Eigen::MatrixXd N;
-  Eigen::MatrixXi E;
+  Graph graph("../data/graphs_test/6_disconnected.obj", opts.verbose);
+  graph.init();
+
+  bool is_connected, is_biconnected, is_triconnected;
+  is_connected = graph.is_connected();
+  is_biconnected = graph.is_biconnected();
+  is_triconnected = graph.is_triconnected();
   
-  readGraphOBJ(opts.path_graph_obj ,N, E);
-  directional::plot_graph(N, E, opts);
+  graph.plot();
 
   return 0;
 }
-
