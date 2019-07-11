dfaf205c5dac3fc872f740d96c4e20a02145525d
rFalque <r.falque@gmail.com>
2019-07-11 11:10:02 +1000

    remove for loop for coeffwise operation

--- src/graph_lib/getMinMax.hpp
+++ src/graph_lib/getMinMax.hpp
@@ -8,21 +8,10 @@
 #define GETMINMAX_HPP
 
 #include <Eigen/Core>
-#include <limits>
 
-inline void getMinMax(Eigen::MatrixXd in_cloud, Eigen::Vector3d & min_point, Eigen::Vector3d & max_point){
-    min_point << std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity();
-    max_point << -std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity(), -std::numeric_limits<double>::infinity();
-
-    for(int i = 0; i < in_cloud.rows(); i++){
-        min_point(0) = std::min(min_point(0), in_cloud.row(i)(0));
-        min_point(1) = std::min(min_point(1), in_cloud.row(i)(1));
-        min_point(2) = std::min(min_point(2), in_cloud.row(i)(2));
-	
-        max_point(0) = std::max(max_point(0), in_cloud.row(i)(0));
-        max_point(1) = std::max(max_point(1), in_cloud.row(i)(1));
-        max_point(2) = std::max(max_point(2), in_cloud.row(i)(2));
-    }
+static inline void getMinMax(Eigen::MatrixXd & in_cloud, Eigen::Vector3d & min_point, Eigen::Vector3d & max_point){
+    max_point = in_cloud.colwise().maxCoeff();
+    min_point = in_cloud.colwise().minCoeff();
 };
 
 inline void getScale(Eigen::MatrixXd in_cloud, double & scale){
