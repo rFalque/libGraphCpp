9baf054ec635d3ced999d0d4488d97d09b3aae33
rFalque <r.falque@gmail.com>
2019-07-16 22:45:40 +1000

    update path of LibIGL

--- src/cmake/FindLIBIGL.cmake
+++ src/cmake/FindLIBIGL.cmake
@@ -14,7 +14,7 @@ find_path(LIBIGL_INCLUDE_DIR igl/readOBJ.h
         ENV LIBIGL_ROOT
         ENV LIBIGL_DIR
     PATHS
-        /home/ubuntu/libigl
+        ~/libigl
         ${CMAKE_SOURCE_DIR}/../..
         ${CMAKE_SOURCE_DIR}/..
         ${CMAKE_SOURCE_DIR}
